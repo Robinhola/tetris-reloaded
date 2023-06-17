ad1fc6f1ea56ac980f57124b2587292c0dfbabba
Robin Elrharbi-Fleury <robin.elrharbi-fleury@gmail.com>
2023-06-17 10:45:30 +0100

    Add movement

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <unordered_set>
 
 // Define the size of each square in the grid
 constexpr float SQUARESIZE = 20.0f;
@@ -94,6 +95,11 @@ void draw(T t, sf::RenderWindow &window) {
     }
 }
 
+T addBlocks(T t, const std::vector<sf::RectangleShape> &blocks) {
+    t.blocks.insert(t.blocks.end(), blocks.begin(), blocks.end());
+    return t;
+}
+
 }  // namespace Blocks
 
 namespace Piece {
@@ -108,7 +114,6 @@ struct T {
 sf::Color color(int type) {
     switch (type) {
         case 1:
-            std::cout << "Debug 1, color red" << std::endl;
             return sf::Color::Red;
         default:
             return sf::Color::White;
@@ -120,7 +125,6 @@ std::vector<sf::RectangleShape> blocks(int type, sf::Vector2f origin,
     std::vector<sf::RectangleShape> blocks;
     switch (type) {
         case 1: {
-            std::cout << "Debug 1, create blocks" << std::endl;
             float x = (origin.x + position.x) * SQUARESIZE;
             float y = (origin.y + position.y) * SQUARESIZE;
 
@@ -165,6 +169,20 @@ T set(T t, int orientation, int type, sf::Vector2f position) {
     return t;
 }
 
+T reset(T t) {
+    int style = 1;
+    return set(t, 0, style, sf::Vector2f(NUMCOLS / 2.0f, 0));
+}
+
+T copyWithOffset(const T &t, sf::Vector2f offset) {
+    T copy = T(t);
+    float offX = t.position.x + (offset.x);
+    float offY = t.position.y + (offset.y);
+    auto offsetPosition = sf::Vector2f(offX, offY);
+
+    return set(copy, t.orientation, t.type, offsetPosition);
+}
+
 T init(sf::Vector2f origin) {
     T t = T{};
     t.origin = origin;
@@ -203,11 +221,57 @@ void draw(T t, sf::RenderWindow &window) {
     Blocks::draw(t.blocks, window);
     Piece::draw(t.piece, window);
 }
+
+bool isPieceColliding(T t, Piece::T piece) {
+    for (const auto &newBlock : piece.blocks) {
+        if (newBlock.getPosition().x <= 0) return true;
+        if (newBlock.getPosition().x > NUMCOLS * SQUARESIZE) return true;
+        if (newBlock.getPosition().y > NUMROWS * SQUARESIZE) return true;
+
+        for (const auto &existingBlock : t.blocks.blocks) {
+            if (newBlock.getPosition() == existingBlock.getPosition()) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+T move(T t, const sf::Vector2f &direction) {
+    Piece::T current = t.piece;
+    Piece::T newPiece = Piece::copyWithOffset(t.piece, direction);
+    if (!isPieceColliding(t, newPiece)) t.piece = newPiece;
+    return t;
+}
+
+T moveLeft(T t) { return move(t, sf::Vector2f(-1, 0)); }
+T moveRight(T t) { return move(t, sf::Vector2f(1, 0)); }
+T moveDown(T t) { return move(t, sf::Vector2f(0, 1)); }
+
+T update(T t) {
+    Piece::T current = t.piece;
+    Piece::T newPiece = Piece::copyWithOffset(t.piece, sf::Vector2f(0, 1));
+
+    if (isPieceColliding(t, newPiece)) {
+        t.blocks = Blocks::addBlocks(t.blocks, current.blocks);
+        t.piece = Piece::reset(current);
+        // remove full lines
+    } else {
+        t.piece = newPiece;
+    }
+
+    return t;
+}
 }  // namespace State
 
 struct TetrisPiece {
     std::vector<sf::RectangleShape> blocks;
 };
+
+constexpr float FixedNumberOfFrames = 60.0f;
+constexpr float FixedTimeStep = 1.0f / FixedNumberOfFrames;
+constexpr float speed = 5.0f;
+constexpr float FramesBeforeFall = FixedNumberOfFrames / speed;
 
 int main() {
     constexpr float window_width = (NUMCOLS + 2 * OFFSET_GRID) * SQUARESIZE;
@@ -217,13 +281,70 @@ int main() {
 
     State::T state = State::init();
 
+    sf::Clock clock;
+    float deltaTime = 0.0f;
+    float accumulatedTime = 0.0f;
+    float accumulatedFramesBeforeFall = 0.0f;
+    std::unordered_set<sf::Keyboard::Key> pressedKeys;
+    auto isPressed = [&pressedKeys](sf::Keyboard::Key key) {
+        return pressedKeys.count(key) > 0;
+    };
+    auto addPressedKey = [&pressedKeys](sf::Keyboard::Key key) {
+        return pressedKeys.insert(key);
+    };
+    auto removePressedKey = [&pressedKeys](sf::Keyboard::Key key) {
+        return pressedKeys.erase(key);
+    };
+
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) window.close();
+
+            if (event.type == sf::Event::KeyPressed) {
+                if (isPressed(event.key.code)) {
+                    continue;
+                }
+
+                if (event.key.code == sf::Keyboard::Left) {
+                    state = State::moveLeft(state);
+                } else if (event.key.code == sf::Keyboard::Right) {
+                    state = State::moveRight(state);
+                } else if (event.key.code == sf::Keyboard::Down) {
+                    state = State::moveDown(state);
+                } else if (event.key.code == sf::Keyboard::Space) {
+                }
+
+                addPressedKey(event.key.code);
+            }
+
+            if (event.type == sf::Event::KeyReleased) {
+                /* if (isPressed(event.key.code)) { */
+                removePressedKey(event.key.code);
+                /* } */
+            }
         }
 
         window.clear(COLOR_BACKGROUND);
+
+        deltaTime = clock.restart().asSeconds();
+        accumulatedTime += deltaTime;
+
+        while (accumulatedTime >= FixedTimeStep) {
+            accumulatedFramesBeforeFall += 1.0f;
+
+            // Good idea but currently does not work
+            /* if (accumulatedFramesBeforeFall >= FramesBeforeFall / 2) { */
+            /*     pressedKeys.clear(); */
+            /* } */
+
+            if (accumulatedFramesBeforeFall >= FramesBeforeFall) {
+                state = State::update(state);
+                accumulatedFramesBeforeFall = 0.0f;
+            }
+
+            accumulatedTime -= FixedTimeStep;
+        }
 
         State::draw(state, window);
 
